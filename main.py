143f3e5c72fb05822d6c0a30810bae62f0fc5d52
hanchchch <cndghks15@gmail.com>
2024-09-21 15:34:41 -0700

    skip already processed

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import logging
+import os
 import sys
 
 from src.example_models import models
 from src.llm import LLM, Input
+
+output_dir = "outputs"
 
 prompt_write_functions = """These are example pytorch code.
 Try writing new pytorch code snippets.
@@ -24,10 +27,16 @@ logger = logging.getLogger()
 
 
 def write_functions(reference_model: str):
+    md_path = f"{output_dir}/learn_from_{reference_model}.md"
+    if os.path.exists(md_path):
+        logger.info(f"{reference_model} has already been processed, skipping")
+        return
+
     i = Input()
     llm = LLM()
 
     modelfiles = models[reference_model]
+
     for filename, content in modelfiles.items():
         prompt = f"file: {filename}\n{content}"
         i.add(prompt)
@@ -36,16 +45,13 @@ def write_functions(reference_model: str):
     logger.info(
         f"Generating functions based on {reference_model} ({len(modelfiles)} files)"
     )
-
     md = ""
-    with open(f"outputs/learn_from_{reference_model}.md", "w") as f:
+    with open(md_path, "w") as f:
         for response in llm.generate(i):
             md += response
             f.write(response)
 
-    logger.info(
-        f"Generated markdown file: outputs/learn_from_{reference_model}.md (token usage: {llm.usage})"
-    )
+    logger.info(f"Generated markdown file: {md_path} (token usage: {llm.usage})")
 
     snippets = [s for s in md.split("###") if s != ""]
     logger.info(f"Generated {len(snippets)} snippets")
@@ -53,7 +59,7 @@ def write_functions(reference_model: str):
         try:
             name = snippet.split("\n")[0].strip().replace(" ", "_").lower()
             code = snippet.split("```python")[1].split("```")[0].strip()
-            with open(f"outputs/{reference_model}_{name}.py", "w") as f:
+            with open(f"{output_dir}/{reference_model}_{name}.py", "w") as f:
                 f.write(code)
         except IndexError:
             logger.warning(f"Failed to parse snippet, model output: {snippet}")
