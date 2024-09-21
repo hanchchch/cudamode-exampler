f5b80540f7b35cb49fd9fc78bd14af0166729479
hanchchch <cndghks15@gmail.com>
2024-09-21 16:17:35 -0700

    separate output dir

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from src.linter import is_valid_python
 from src.llm import LLM, Input
 
 output_dir = "outputs"
+output_md_dir = "outputs/md"
+output_py_dir = "outputs/py"
 
 prompt_write_functions = """These are example pytorch code.
 Try writing new pytorch code snippets.
@@ -28,7 +30,7 @@ logger = logging.getLogger()
 
 
 def write_functions(reference_model: str):
-    md_path = f"{output_dir}/learn_from_{reference_model}.md"
+    md_path = f"{output_md_dir}/learn_from_{reference_model}.md"
     if os.path.exists(md_path):
         logger.info(f"{reference_model} has already been processed, skipping")
         return
@@ -54,23 +56,26 @@ def write_functions(reference_model: str):
 
     logger.info(f"Generated markdown file: {md_path} (token usage: {llm.usage})")
 
-    filepaths = []
     snippets = [s for s in md.split("###") if s != ""]
     logger.info(f"Generated {len(snippets)} snippets")
     for snippet in snippets:
         try:
             name = snippet.split("\n")[0].strip().replace(" ", "_").lower()
             code = snippet.split("```python")[1].split("```")[0].strip()
-            filepath = f"{output_dir}/{reference_model}_{name}.py"
-            with open(filepath, "w") as f:
-                f.write(code)
-            if not is_valid_python([filepath]):
-                logger.warning(f"Invalid python code: {code}")
-                os.remove(filepath)
-                continue
         except IndexError:
             logger.warning(f"Failed to parse snippet, model output: {snippet}")
             continue
+
+        filepath = f"{output_py_dir}/{reference_model}_{name}.py"
+        with open(filepath, "w") as f:
+            f.write(code)
+
+        if not is_valid_python([filepath]):
+            logger.warning(f"Invalid python code: {code}")
+            os.remove(filepath)
+            continue
+
+        logger.info(f"Saved snippet to {filepath}")
 
 
 if __name__ == "__main__":
