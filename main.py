f0bf44d83dabb5c26ee772e257d2b955b88292d2
hanchchch <cndghks15@gmail.com>
2024-09-21 16:13:23 -0700

    linter

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 from src.example_models import models
+from src.linter import is_valid_python
 from src.llm import LLM, Input
 
 output_dir = "outputs"
@@ -53,19 +54,29 @@ def write_functions(reference_model: str):
 
     logger.info(f"Generated markdown file: {md_path} (token usage: {llm.usage})")
 
+    filepaths = []
     snippets = [s for s in md.split("###") if s != ""]
     logger.info(f"Generated {len(snippets)} snippets")
     for snippet in snippets:
         try:
             name = snippet.split("\n")[0].strip().replace(" ", "_").lower()
             code = snippet.split("```python")[1].split("```")[0].strip()
-            with open(f"{output_dir}/{reference_model}_{name}.py", "w") as f:
+            filepath = f"{output_dir}/{reference_model}_{name}.py"
+            with open(filepath, "w") as f:
                 f.write(code)
+            if not is_valid_python([filepath]):
+                logger.warning(f"Invalid python code: {code}")
+                os.remove(filepath)
+                continue
         except IndexError:
             logger.warning(f"Failed to parse snippet, model output: {snippet}")
             continue
 
 
 if __name__ == "__main__":
+    i = 0
     for model in models.keys():
+        i += 1
+        logger.info(f"--- {i}/{len(models)}: Processing {model}")
         write_functions(model)
+        break
